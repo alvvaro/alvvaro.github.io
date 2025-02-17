33c2198c64b7dc01df667f7b32a939243fe81d9b
álvaro s <50526113+alvvaro@users.noreply.github.com>
2025-02-17 19:53:24 +0100

    init react

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+
+import "@fontsource-variable/inter";
+import "@fontsource/inter";
+
+import ReactDOM from "react-dom/client";
+
+// import Router from "@/pages/Router";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    {/* <Router /> */}
+    <div>hi from react</div>
+  </StrictMode>,
+);
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import eslintPlugin from "@nabla/vite-plugin-eslint";
+import react from "@vitejs/plugin-react-swc";
+
+import { URL, fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), eslintPlugin()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "build",
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
+});
