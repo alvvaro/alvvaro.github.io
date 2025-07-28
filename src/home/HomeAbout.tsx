0c0b612a1b14e149ac2f886ca48ebc1a9d3c7806
álvaro s <50526113+alvvaro@users.noreply.github.com>
2025-07-28 21:02:42 +0200

    update HomeAbout print text

--- src/home/HomeAbout.tsx
+++ src/home/HomeAbout.tsx
@@ -1,21 +1,24 @@
 export default function About() {
   return (
-    <div>
-      <h2 id="About">About</h2>
-      <p className="mt-4">
-        I’m a designer and front-end developer with an interest on
-        human-computer interaction, user experience and product engineering.
-      </p>
-      <p>
-        Currently <a href="#experience">Main React Engineer at Viafirma</a>, a
-        cloud-based electronic signature and digital identity management suite.
-      </p>
-      <div className="hidden print:block">
+    <>
+      <div>
+        <h2 id="About">About</h2>
         <p className="mt-4">
-          ¶ Get the live, full version of this document over on{' '}
-          <a href="https://alvaro.gs/">my website</a>.
+          I’m a designer and front-end developer with an interest on
+          human-computer interaction, user experience and product engineering.
+        </p>
+        <p>
+          Currently <a href="#experience">Main React Engineer at Viafirma</a>, a
+          cloud-based electronic signature and digital identity management
+          suite.
         </p>
       </div>
-    </div>
+      <div className="hidden print:block">
+        <p>
+          {'=>'} Get the live, full version of this document over on{' '}
+          <a href="https://alvaro.gs/">alvaro.gs</a>.
+        </p>
+      </div>
+    </>
   );
 }
