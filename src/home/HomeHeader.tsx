374663f9ee2466f9884293f712c6952a05aa206e
álvaro s <50526113+alvvaro@users.noreply.github.com>
2025-07-28 22:07:15 +0200

    make print avatar smaller

--- src/home/HomeHeader.tsx
+++ src/home/HomeHeader.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <figure className="relative">
         <img
           src="/img/avatar.png"
-          className="w-[96px] rounded-full print:w-[82px]"
+          className="w-[96px] rounded-full print:w-[64px]"
           alt="My picture!"
           title="Despite everything, it's still me."
         />
